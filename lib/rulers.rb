f73ad36b68c3b078af2ec05a95a2f348d6ac19fa
blueplanet <erguolinge@gmail.com>
2016-02-01 07:02:15 +0900

    Add view render

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require 'rulers/version'
 require 'rulers/routing'
 require 'rulers/util'
 require 'rulers/dependencies'
+require 'rulers/controller'
 
 module Rulers
   class Application
@@ -19,16 +20,6 @@ module Rulers
         { 'Content-Type' => 'text/html' },
         [text]
       ]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
